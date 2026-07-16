a2269d2bd2557f33201e0f0f234ddb0ef8d1432c
Tomás Silva Sousa <tomas.sousa@twinstudio.pt>
2026-07-16 10:36:29 +0100

    Adds gitignore and creates the runtime plugin

--- Source/SimpleUECharts/Private/SimpleUECharts.cpp
+++ Source/SimpleUECharts/Private/SimpleUECharts.cpp
@@ -1,7 +1,19 @@
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogSimpleUECharts, Log, All);
+
 class FSimpleUEChartsModule : public IModuleInterface
 {
+public:
+    virtual void StartupModule() override
+    {
+        UE_LOG(LogSimpleUECharts, Log, TEXT("SimpleUECharts runtime module loaded."));
+    }
+
+    virtual void ShutdownModule() override
+    {
+        UE_LOG(LogSimpleUECharts, Log, TEXT("SimpleUECharts runtime module unloaded."));
+    }
 };
 
 IMPLEMENT_MODULE(FSimpleUEChartsModule, SimpleUECharts)
